4f145610d8d076353b3db9e33367d9c13860b38b
Dinonychus <72425800+Dinonychus@users.noreply.github.com>
2023-01-26 16:51:57 -0800

    first commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,78 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.SpearConstants;
+import frc.robot.commands.ExampleCommand;
+// import frc.robot.commands.SpearManualControl;
+import frc.robot.commands.JoystickControl;
+import frc.robot.commands.SetSpearPos;
+import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.SpearPID;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final DriveTrain driveTrain = new DriveTrain();
+  private final SpearPID spearPID = new SpearPID();
+
+
+  private final XboxController driveController = new XboxController(Constants.driveController_ID);
+  private final XboxController mechanismController = new XboxController(Constants.mechanismController_ID);
+  private final CommandXboxController mechanism1Controller = new CommandXboxController(Constants.mechanismController_ID);
+
+  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+
+
+    /*driveTrain.setDefaultCommand(new JoystickControl(driveTrain, 
+    () -> driveController.getRawAxis(Constants.LeftY), 
+    () -> driveController.getRawAxis(Constants.rightX))); */
+     //spearPID.setDefaultCommand(new SetSpearPos(0, spearPID));
+
+
+    configureButtonBindings();
+  }
+
+
+  private void configureButtonBindings() {
+    
+   // final JoystickButton kLeftBumper = new JoystickButton(mechanism1Controller, Constants.kLeft_Bumper);
+    // final JoystickButton kRightBumper = new JoystickButton(mechanism1Controller, Constants.kRight_Bumper);
+
+
+    mechanism1Controller.leftBumper().onTrue(new SetSpearPos(
+      SpearConstants.kSpearRetract, spearPID));
+
+    mechanism1Controller.rightBumper().onTrue(new SetSpearPos(
+      SpearConstants.kSpearExtend, spearPID));
+
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return m_autoCommand;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,72 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.SpearConstants;
+
+public class DriveTrain extends SubsystemBase {
+  
+  private CANSparkMax frontLeftMotor = new CANSparkMax(DriveConstants.kFLDriveID, MotorType.kBrushless);
+  private CANSparkMax frontRightMotor = new CANSparkMax(DriveConstants.kFRDriveID, MotorType.kBrushless);
+  private CANSparkMax backLeftMotor = new CANSparkMax(DriveConstants.kBLDriveID, MotorType.kBrushless);
+  private CANSparkMax backRightMotor = new CANSparkMax(DriveConstants.kBRDriveID, MotorType.kBrushless);
+
+ /* private TalonFX frontLeftFalconMotor = new TalonFX(DriveConstants.kFLFalconDriveID);
+  private TalonFX backLeftFalconMotor = new TalonFX(DriveConstants.kBLFalconDriveID);
+  private TalonFX frontRightFalconMotor = new TalonFX(DriveConstants.kFRFalconDriveID);
+  private TalonFX backRightFalconMotor = new TalonFX(DriveConstants.kBRFalconDriveID);
+  private TalonFX extraMotor1ID = new TalonFX(DriveConstants.extraMotor1);
+ // private TalonFX extraMotor2ID = new TalonFX(DriveConstants.extraMotor2); */ 
+
+ // private CANSparkMax spearTestMotor = new CANSparkMax(SpearConstants.kSpearDriveID, MotorType.kBrushless);
+
+
+  private MotorControllerGroup m_leftSide = new MotorControllerGroup(frontLeftMotor, backLeftMotor);
+  private MotorControllerGroup m_rightSide = new MotorControllerGroup(frontRightMotor, backRightMotor);
+
+  //private MotorControllerGroup LeftSideFalcon = new MotorControllerGroup(frontLeftFalconMotor, backLeftFalconMotor);
+
+
+
+
+
+
+
+  public DriveTrain() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void setMotorSpeed (double leftSpeed, double rightSpeed){
+    m_leftSide.set(leftSpeed);
+    m_rightSide.set(rightSpeed);
+    
+  }
+
+ /*  public void setSpearMotorSpeed (double spearSpeed){
+    spearTestMotor.set(spearSpeed);
+  } */
+
+ /* public void setFalconMotorSpeed (double leftFalconSpeed, double rightFalconSpeed){
+    frontLeftFalconMotor.set(TalonFXControlMode.PercentOutput, leftFalconSpeed);
+    backLeftFalconMotor.set(TalonFXControlMode.PercentOutput, rightFalconSpeed);
+    frontRightFalconMotor.set(TalonFXControlMode.PercentOutput, leftFalconSpeed);
+    backRightFalconMotor.set(TalonFXControlMode.PercentOutput, rightFalconSpeed);
+    extraMotor1ID.set(TalonFXControlMode.PercentOutput, leftFalconSpeed);
+   // extraMotor2ID.set(TalonFXControlMode.PercentOutput, rightFalconSpeed);
+  } */ 
+
+}
